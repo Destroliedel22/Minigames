f4d84173248af3e1f627f6bf0736f5047d4e203b
Destroliedel22 <Jordiwalet@gmail.com>
2025-04-15 14:36:02 +0200

    board 11x11 set

---
+++ Nonogram/Nonogram.js
@@ -0,0 +1,42 @@
+let tile;
+let tiles = [];
+
+window.onload = function()
+{
+    setGame();
+}
+
+function setGame()
+{
+    for(let i = 0; i < 11; i++)
+    {
+        for(let j = 0; j < 11; j++)
+        {
+            tile = document.createElement("div");
+            tile.id = i.toString() + "-" + j.toString();
+            if(i == 0 || j == 0)
+            {
+                //row filled count
+            }
+            else
+            {
+                tile.addEventListener("click", selectTile);
+            }
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+            tiles.push(tile);
+        }
+    }
+}
+
+function selectTile()
+{
+    if(this.classList.contains("tileSelected"))
+    {
+        this.classList.remove("tileSelected");
+    }
+    else
+    {
+        this.classList.add("tileSelected");
+    }
+}
