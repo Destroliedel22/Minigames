1dfeea5c9a515a5efd62ddf175d1706670cbe4a7
Destroliedel22 <Jordiwalet@gmail.com>
2025-05-03 14:17:09 +0200

    added win and the board is solvable and you can cross tiles

--- Nonogram/Nonogram.js
+++ Nonogram/Nonogram.js
@@ -7,6 +7,10 @@ let colTile;
 let x;
 let y;
 
+let solution = [];
+let rowClues = [];
+let colClues = [];
+
 window.onload = function()
 {
     setGame();
@@ -14,28 +18,71 @@ window.onload = function()
 
 function setGame()
 {
+    for (let i = 0; i < 10; i++) 
+    {
+        let row = [];
+        for (let j = 0; j < 10; j++) {
+            row.push(Math.random() < 0.5 ? 1 : 0);
+        }
+        solution.push(row);
+        console.log(solution);
+    }
+
+    function getClues(line) {
+        let clues = [];
+        let count = 0;
+        for (let cell of line) {
+            if (cell === 1) {
+                count++;
+            } else if (count > 0) {
+                clues.push(count);
+                count = 0;
+            }
+        }
+        if (count > 0) clues.push(count);
+        return clues.length > 0 ? clues.join(" ") : "0";
+    }
+
+    rowClues = solution.map(row => getClues(row));
+
+    for (let j = 0; j < 10; j++) {
+        let col = [];
+        for (let i = 0; i < 10; i++) {
+            col.push(solution[i][j]);
+        }
+        colClues.push(getClues(col));
+    }
+
     for(let i = 0; i < 11; i++)
     {
         for(let j = 0; j < 11; j++)
         {
             tile = document.createElement("div");
             tile.id = i.toString() + "-" + j.toString();
-            if(i == 0 || j == 0)
+            tile.classList.add("tile");
+            if(i === 0)
             {
-                //row filled count
                 tile.classList.add("fillCounter");
-                if(!i == 0 || !j == 0)
-                {
-                    tile.innerText = Math.floor(Math.random() * 10);
-                }
+                tile.innerText = colClues[j - 1];
+            }
+            else if(j === 0)
+            {
+                tile.classList.add("fillCounter");
+                tile.innerText = rowClues[i - 1];
             }
             else
             {
                 tile.addEventListener("click", selectTile);
-                tile.addEventListener("click", Checkrow);
+                tile.addEventListener("contextmenu", crossed)
+                tile.dataset.solution = solution[i - 1][j - 1];
             }
-            tile.classList.add("tile");
-            document.getElementById("board").append(tile);
+
+            if (i === 0 && j === 0)
+            {
+                tile.innerText = ""
+            }
+            
+            board.append(tile);
             tiles.push(tile);
         }
     }
@@ -43,69 +90,100 @@ function setGame()
 
 function selectTile()
 {
-    if(this.classList.contains("tileSelected"))
-    {
-        this.classList.remove("tileSelected");
-    }
-    else
-    {
-        this.classList.add("tileSelected");
-    }
-}
-
-function Checkrow()
-{
     let coords = this.id.split("-");
     x = parseInt(coords[0]);
     y = parseInt(coords[1]);
 
-    // Get the tile at i-0 (same row, first column)
-    let rowTileId = x + "-0";
-    rowTile = document.getElementById(rowTileId);
-
-    // Get the tile at 0-j (first row, same column)
-    let colTileId = "0-" + y;
-    colTile = document.getElementById(colTileId);
-
     if(this.classList.contains("tileSelected"))
     {
-        rowTile.innerText--;
-        colTile.innerText--;
+        this.classList.remove("tileSelected");
     }
-    else
+    else if(!this.classList.contains("Crossed"))
     {
-        rowTile.innerText++;
-        colTile.innerText++;
+        this.classList.add("tileSelected");
     }
 
-    CheckZero();
+    // let splitinnertext = document.getElementById(x + "-" + 0).innerText;
+    // console.log(document.getElementById(x + "-" + 0));
+
+    // if(checkRowAutoCross(x) === document.getElementById(x + "-" + 0))
+    // {
+    //     for (let col = 1; col <= 10; col++) 
+    //         {
+    //             let tile = document.getElementById(x + "-" + col);
+    //             if(!tile.classList.contains("tileSelected")) 
+    //             {
+    //                 tile.classList.add("Crossed");
+    //             }
+    //         }
+    // }
+
+    // checkColAutoCross(y);
+
+    if(checkIfBoardIsCorrect())
+    {
+        document.getElementById("Win").hidden = false;
+        document.getElementById("body").style.backgroundColor = "rgb(13, 88, 0)"
+    }
 }
 
-function CheckZero()
+function crossed(e)
 {
-    if (rowTile.innerText <= "0") {
-        for (let col = 1; col < 11; col++) {
-            let tile = document.getElementById(x + "-" + col);
-            if(!tile.classList.contains("tileSelected"))
-            {
-                tile.classList.add("Crossed");
-                tile.removeEventListener("click", selectTile);
-                tile.removeEventListener("click", CheckZero);
-            }
-        }
-    }
-    else
+    e.preventDefault();
+    if(this.classList.contains("Crossed"))
     {
-        tile.classList.remove("Crossed");
+        this.classList.remove("Crossed");
     }
+    else if(!this.classList.contains("tileSelected"))
+    {
+        this.classList.add("Crossed");
+    }
+}
 
-    if (colTile.innerText <= "0") {
-        for (let row = 1; row < 11; row++) {
-            let tile = document.getElementById(row + "-" + y);
-            if(!tile.classList.contains("tileSelected"))
+function checkIfBoardIsCorrect()
+{
+    for (let i = 1; i <= 10; i++)
+    {
+        for (let j = 1; j <= 10; j++) 
+        {
+            let tile = document.getElementById(i + "-" + j);
+            let expected = solution[i - 1][j - 1];
+
+            if ((expected === 1 && !tile.classList.contains("tileSelected")) || (expected === 0 && tile.classList.contains("tileSelected"))) 
             {
-                tile.classList.add("Crossed");
+                return false;
             }
         }
     }
+    return true;
+}
+
+function checkRowAutoCross(rowIndex) 
+{
+    let filledCount = 0;
+
+    for (let col = 1; col <= 10; col++) 
+    {
+        let tile = document.getElementById(rowIndex + "-" + col);
+        if (tile.classList.contains("tileSelected")) 
+        {
+            filledCount++;
+        }
+    }
+    return filledCount;
+}
+
+function checkColAutoCross(colIndex) 
+{
+    let filledCount = 0;
+
+    for (let row = 1; row <= 10; row++) 
+    {
+        let tile = document.getElementById(row + "-" + colIndex);
+        if (tile.classList.contains("tileSelected")) 
+        {
+            filledCount++;
+        }
+    }
+    return filledCount;
 }
